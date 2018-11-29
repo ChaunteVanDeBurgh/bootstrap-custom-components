fad80d830ec61c5f254b9ff914659d6dfa60b116
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-11-28 22:02:52 -0600

    create template variables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$('#githubModal').on('show.bs.modal', function (e) {
 		var $element = $(this),
-			url = 'https://api.github.com/users/{username}';
+			url = 'https://api.github.com/users/{username}',
+			title = 'Hi, my name is {name}',
+			content = '' +
+				'<div class="row">' +
+					'<img src="{img}" class="col-sm-3">' +
+					'<p class="col-sm-9" id="bio">{bio}</p>' +
+					'</div>',
+			bio = '' + 
+				'At the moment I have {publicRepo} public repos' +
+				'and {folowers} followers. /n' +
+				'I joined Github on {dateJoin}';
 
 		url = url.replace(/{username}/, $('#github-username').val());
 
